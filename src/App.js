3a1264cba961c816d210b2636b120ffb8cf65482
Benjamin Lee <benjiman0125@hotmail.com>
2022-07-17 17:18:48 +0800

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState, useEffect, useLayoutEffect, useCallback, useMemo } from "react";
+import axios from "axios";
+
+import FirstPhase from "./components/firstPhase";
+import SecondPhase from "./components/secondPhase";
+import ThirdPhase from "./components/thirdPhase";
+
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [selectByName, setSelectByName] = useState(false)
+  const [allData1, setAllData1] = useState([]);
+  const [allData2, setAllData2] = useState([]);
+  const [allData3, setAllData3] = useState([]);
+  const [nameStage1, setNameStage1] = useState([]);
+  const [nameStage2, setNameStage2] = useState([]);
+  const [nameStage3, setNameStage3] = useState([]);
+
+  const fetchAllTask = () => {
+    axios.get(`http://localhost:3001/task`)
+      .then((res) => {
+        setAllData1(res.data[0]['Stage 1'])
+        setAllData2(res.data[0]['Stage 2'])
+        setAllData3(res.data[0]['Stage 2'])
+      })
+      .catch((err) => {
+        console.log("Err in fetchAllTask: ", err);
+      });
+  }
+
+  const fetchByName = (name) => {
+    axios.get(`http://localhost:3001/task/select-by-name?name=${name}`)
+      .then((res) => {
+        setSelectByName(true);
+        setNameStage1(res.data[0]['Stage 1']);
+        setNameStage2(res.data[0]['Stage 2']);
+        setNameStage3(res.data[0]['Stage 3']);
+      })
+      .catch((err) => {
+        console.log("Err in fetchByName: ", err);
+      });
+  }
+
+  useLayoutEffect(() => {
+    if (name) {
+      fetchByName(name)
+    }
+  }, [name])
+
+  useLayoutEffect(() => {
+    fetchAllTask()
+  }, [])
+
+  useEffect(() => {
+    if(name === 'All') {
+      setSelectByName(false)
+      fetchAllTask()
+    }
+  }, [name])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Wireframe</h1>
+        <div className="row">
+          <h3 className="col-9"> Task</h3>
+
+          <div className="col-3">
+            <select class="form-select" id="name" value={name}
+              onChange={(e) => 
+              setName(e.target.value)
+              }>
+              <option value="All">All assignee</option>
+              <option value="David">David</option>
+              <option value="Perry">Perry</option>
+              <option value="Tommy">Tommy</option>
+              <option value="Tony">Tony</option>
+              <option value="Sandy">Sandy</option>
+              <option value="Allen">Allen</option>
+              <option value="Mathew">Mathew</option>
+              <option value="Mat">Mat</option>
+            </select>
+            <p>Filtered by assignee: {name}</p>
+          </div>
+        </div>
+
+        <div class="container">
+        {selectByName === false ? <FirstPhase data={allData1} /> : <FirstPhase data={nameStage1} />}
+        <p><br /></p>
+        {selectByName === false ? <SecondPhase data={allData2} /> : <SecondPhase data={nameStage2} />}
+        <p><br /></p>
+        {selectByName === false ? <ThirdPhase data={allData3} /> : <ThirdPhase data={nameStage3} />}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/firstPhase.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Box, Card, CardContent, Button, Typography } from '@mui/material';
+
+const FirstPhase = (props) => {
+
+    const renderList = props.data.map((products) => {
+        const { by_who, description, priority, stage, task } = products
+        const cardStyle = {
+            display: "block",
+            transitionDuration: "0.3s",
+            width: "24vw",
+        };
+        return (
+            <>
+                <Box component="span" sx={{ display: 'inline-block', transform: 'scale(0.8)', flexFlow: 1 }}>
+                    <Card style={cardStyle} sx={{ minWidth: 10 }}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h4" component="div">
+                                {task}
+                            </Typography>
+                            <Typography>
+                                {description}
+                                <br />
+                                <br />
+                                <br />
+                            </Typography>
+                            <div className="row">
+                                <div className="col-6">
+                                    Assignee name: {by_who}
+                                </div>
+                                <div className="col-6">
+                                    <Button variant="contained" size="small">Priority:{priority}</Button>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Box>
+            </>
+        )
+    })
+
+    return (
+        <>
+            <div class="row">
+                <div className="stage1-left col-2" >
+                    <h1>Stage 1</h1>
+                </div>
+                <div className="stage1-right col-10">
+                    {renderList}
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default FirstPhase
